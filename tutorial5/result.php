9f17ccf509219303eaf74a5ff40d9f303c7269a5
Jinal Rajpara <jrajpara219@rku.ac.in>
2021-12-20 14:31:23 -0500

    Update result.php

--- tutorial5/result.php
+++ tutorial5/result.php
@@ -2,7 +2,7 @@
 //echo "<pre>";
 //print_r($_POST);
 //echo "<pre>";
-echo ($_POST['month'])?$_POST['month']:0;
+$m=$_POST['month']
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,18 +16,18 @@ echo ($_POST['month'])?$_POST['month']:0;
     <div class="container">
      <div class="row">
          <select name="month" id="" require>
-             <option <?= ($m=="January")?"selected":"">January</option>
-             <option <?= ($m=="Fabruary")?"selected":"">Fabruary</option>
-             <option <?= ($m=="March")?"selected":"">March</option>
-             <option <?= ($m=="April")?"selected":"">April</option>
-             <option <?= ($m=="May")?"selected":"">May</option>
-             <option <?= ($m=="June")?"selected":"">June</option>
-             <option <?= ($m=="July")?"selected":"">July</option>
-             <option <?= ($m=="August")?"selected":"">August</option>
-             <option <?= ($m=="September")?"selected":"">September</option>
-             <option <?= ($m=="October")?"selected":"">October</option>
-             <option <?= ($m=="November")?"selected":"">November</option>
-             <option <?= ($m=="December")?"selected":"">December</option>
+             <option <?= ($m==1)?"selected":"">January</option>
+             <option <?= ($m==2)?"selected":"">Fabruary</option>
+             <option <?= ($m==3)?"selected":"">March</option>
+             <option <?= ($m==4)?"selected":"">April</option>
+             <option <?= ($m==5)?"selected":"">May</option>
+             <option <?= ($m==6)?"selected":"">June</option>
+             <option <?= ($m==7)?"selected":"">July</option>
+             <option <?= ($m==8)?"selected":"">August</option>
+             <option <?= ($m==9)?"selected":"">September</option>
+             <option <?= ($m==10)?"selected":"">October</option>
+             <option <?= ($m==11)?"selected":"">November</option>
+             <option <?= ($m==12)?"selected":"">December</option>
          </select>
      </div>
     </div>
